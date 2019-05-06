c277689cb48054dc0c9d6fb8b98b3f2438c3550c
ftnext <takuyafjp+develop@gmail.com>
2019-05-06 12:05:43 +0900

    [feat] Photoモデルのidをuuidに変更（__str__メソッドでuuidを表示）

--- django/blog/models.py
+++ django/blog/models.py
@@ -47,11 +47,14 @@ class Category(models.Model):
 
 def uploaded_image_path(instance, filename):
     # アップロード先：MEDIA_ROOT/photos/<uuid>.png
-    uuid_filename = uuid4()
     image_extension = filename.split('.')[-1]
-    return f'photos/{uuid_filename}.{image_extension}'
+    return f'photos/{instance.id}.{image_extension}'
 
 
 class Photo(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True, editable=False)
     image = models.ImageField('画像', upload_to=uploaded_image_path)
     uploaded_date = models.DateTimeField('アップロード日', auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.id}'
