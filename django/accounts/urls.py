a5bedefc7fcce99784c2cc7a358c0e3a14a64bc4
ftnext <takuyafjp+develop@gmail.com>
2019-05-06 15:20:08 +0900

    [feat] ログイン・ログアウトの機能を実装
    * Django Girls Tutorial Extensionsにならった
    * フッターのアイコンからの遷移先は管理サイトのログイン画面とした

--- django/accounts/urls.py
+++ django/accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
@@ -6,4 +7,9 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/',
+         auth_views.LoginView.as_view(template_name='accounts/login.html'),
+         name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'),
+         name='logout'),
 ]
