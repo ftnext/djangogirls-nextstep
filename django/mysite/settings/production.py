c2288c84fb08f02050b2e46d46e7d83b155a0a6a
ftnext <takuyafjp+develop@gmail.com>
2019-05-12 21:41:08 +0900

    [chore] python-dotenvを導入し、SECRET_KEYを.envファイルから読み込むように設定
    * ref: https://help.pythonanywhere.com/pages/environment-variables-for-web-apps/

--- django/mysite/settings/production.py
+++ django/mysite/settings/production.py
@@ -1,9 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+
 from .base import (
     ALLOWED_HOSTS,
 )
 from .base import * # NOQA
 
 
+load_dotenv()
+
 DEBUG = False
 
 ALLOWED_HOSTS += ['.pythonanywhere.com']
+
+SECRET_KEY = os.getenv('SECRET_KEY')
